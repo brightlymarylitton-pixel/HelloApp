6a08447e4a3f94190d12158932c4657f8dd3e34e
Brightly Mary Litton <brightlymarylitton@gmail.com>
2026-03-09 20:47:59 +0300

    UC4 Added

---
+++ OOPSBannerAppUC4.java
@@ -0,0 +1,20 @@
+public class OOPSBannerAppUC4 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            "   ***     ***    ******    ***** ",
+            "  ** **   ** **   **   **  **   **",
+            " **   ** **   **  **   ** **      ",
+            " **   ** **   **  ******   *****  ",
+            " **   ** **   **  **            **",
+            "  ** **   ** **   **      **   ** ",
+            "   ***     ***    **       *****  "
+        };
+
+        for (int i = 0; i < banner.length; i++) {
+            System.out.println(banner[i]);
+        }
+
+    }
+}
